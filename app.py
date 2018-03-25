7c9fe49ffbbe5568f7446f047bda05401ba66d53
Loice Meyo <loicemeyo@gmail.com>
2018-03-25 17:12:07 +0300

    Add, Update, Delete and Get endpoints for books

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, request
+from flask import Flask, request, Response, json
 app=Flask (__name__)
 
-@app.route('/api/books', methods=['GET','POST','DELETE','PUT'])
-def save():
+books = {}
+
+@app.route('/api/books', methods=['POST'])
+def save_book():
+    if request.method == 'POST':
+        book_name = request.form["name"]
+        current_count = len(books)
+        books[current_count + 1] = book_name
+        return Response(json.dumps({'message': 'Successfully posted!'}), status=201, mimetype='application/json')
+
+@app.route('/api/books/<book_id>', methods=['GET', 'DELETE', 'PUT'])
+def access_book(book_id):
+    """This method accesses a book and performs an operation to it depending on the request method"""
     if request.method == 'GET':
-        return "Looks good!"
+        try:
+            print("le books!",books, book_id)
+            book = books[int(book_id)]
+            print("retrieved book", books[int(book_id)])
+            return Response(json.dumps({'book': book}), status=200, mimetype='application/json')
+        except KeyError:
+            return Response(json.dumps({'message': 'Invalid book Id'}), status=404, mimetype='application/json')
+
+    if request.method == 'PUT':
+        new_book_name = request.form['name']
+        try:
+            book = books[int(book_id)]
+            if book:
+                books[int(book_id)] = new_book_name
+                return Response(json.dumps({'message':'Updated Successfully'}), status=200, mimetype='application/json')
+            else:
+               return Response(json.dumps({'message': 'Invalid book Id'}), status=404, mimetype='application/json') 
+        except KeyError:
+            return Response(json.dumps({'message': 'Invalid book Id'}), status=404, mimetype='application/json')
+
+    if request.method=='DELETE':
+        try:
+            book = books[int(book_id)]
+            if book:
+                del books[int(book_id)]
+                return Response(json.dumps({'message':'Deleted Successfully'}), status=204, mimetype='application/json')
+            else:
+                return Response(json.dumps({'message': 'Invalid book Id'}), status=404, mimetype='application/json') 
+        except KeyError:
+            return Response(json.dumps({'message': 'Invalid book Id'}), status=404, mimetype='application/json')  
+
     
 if __name__=='__main__':
     app.run()
